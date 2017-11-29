1bd0b54e37afc4c7aa042e665dcd53960d7f9e05
Nycholas <nycholas@live.com>
2017-11-29 04:11:34 -0300

    Ajustes no Factory Method

--- src/business/control/questions/factory/QuestionsFactory.java
+++ src/business/control/questions/factory/QuestionsFactory.java
@@ -7,39 +7,8 @@ import business.model.questions.Questions;
 import infra.adapter.CsvImport;
 import java.sql.SQLException;
 
-public abstract class QuestionsFactory {
+public interface QuestionsFactory {
 
-    public Questions createQuestionFromJson(String importFile) throws SQLException {
-        try {
-            MysqlConnect mysql = MysqlConnect.getDbCon();
-            AdapterImport adapter = new JsonImport();
-            Questions question = adapter.parseData(importFile);
+    public Questions createQuestion(String importFile) throws SQLException;
 
-            mysql.insert("INSERT INTO `questions` (`statment`, `alternative1`, `alternative2`, `alternative3`, `alternative4`, `alternative5`, `correct`) VALUES "
-                    + "('" + question.getStatment() + "', '" + question.getAlternative1() + "', '" + question.getAlternative2() + "', '" + question.getAlternative3() + "', '"
-                    + question.getAlternative4() + "', '" + question.getAlternative5() + "', '" + question.getCorrect() + "')");
-
-            return question;
-        } catch (SQLException error) {
-            System.out.println("Error: " + error);
-        }
-        return null;
-    }
-    
-    public Questions createQuestionFromCsv(String importFile) throws SQLException {
-        try {
-            MysqlConnect mysql = MysqlConnect.getDbCon();
-            AdapterImport adapter = new CsvImport();
-            Questions question = adapter.parseData(importFile);
-
-            mysql.insert("INSERT INTO `questions` (`statment`, `alternative1`, `alternative2`, `alternative3`, `alternative4`, `alternative5`, `correct`) VALUES "
-                    + "('" + question.getStatment() + "', '" + question.getAlternative1() + "', '" + question.getAlternative2() + "', '" + question.getAlternative3() + "', '"
-                    + question.getAlternative4() + "', '" + question.getAlternative5() + "', '" + question.getCorrect() + "')");
-
-            return question;
-        } catch (SQLException error) {
-            System.out.println("Error: " + error);
-        }
-        return null;
-    }
 }
---
+++ src/business/control/questions/factory/QuestionsFromCsv.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package business.control.questions.factory;
+
+import business.model.questions.Questions;
+import infra.adapter.AdapterImport;
+import infra.adapter.CsvImport;
+import infra.database.MysqlConnect;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author nycho
+ */
+public class QuestionsFromCsv {
+    public Questions createQuestion(String importFile) throws SQLException {
+        try {
+            MysqlConnect mysql = MysqlConnect.getDbCon();
+            AdapterImport adapter = new CsvImport();
+            Questions question = adapter.parseData(importFile);
+
+            mysql.insert("INSERT INTO `questions` (`statment`, `alternative1`, `alternative2`, `alternative3`, `alternative4`, `alternative5`, `correct`) VALUES "
+                    + "('" + question.getStatment() + "', '" + question.getAlternative1() + "', '" + question.getAlternative2() + "', '" + question.getAlternative3() + "', '"
+                    + question.getAlternative4() + "', '" + question.getAlternative5() + "', '" + question.getCorrect() + "')");
+
+            return question;
+        } catch (SQLException error) {
+            System.out.println("Error: " + error);
+        }
+        return null;
+    }
+}
