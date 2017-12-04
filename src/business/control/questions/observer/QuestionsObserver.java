a45778d05575f82e09ee2e63fede3bed1e106411
Nycholas <nycholas@live.com>
2017-12-04 14:17:23 -0300

    Observer Enviando Email. Closes #15

--- src/business/control/questions/observer/QuestionsObserver.java
+++ src/business/control/questions/observer/QuestionsObserver.java
@@ -1,5 +1,16 @@
 package business.control.questions.observer;
 
+import business.control.StudentControl;
+import java.util.Properties;
+import javax.mail.Address;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
 public class QuestionsObserver extends Observer {
 
     public QuestionsObserver(QuestionsSubject questions) {
@@ -8,7 +19,45 @@ public class QuestionsObserver extends Observer {
 
     @Override
     public void update() {
-        System.out.println("Novas Questões Adicionadas! Total de questoes: " + questions.getState());
+        Properties props = new Properties();
+
+        //Parâmetros de conexão com servidor Gmail
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.socketFactory.port", "465");
+        props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.port", "465");
+
+        Session session = Session.getDefaultInstance(props,
+                new javax.mail.Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication("email@gmail.com", "password");
+            }
+        });
+
+        session.setDebug(true);
+
+        try {
+
+            Message message = new MimeMessage(session);
+            //Remetente
+            message.setFrom(new InternetAddress("email@gmail.com"));
+
+            //Destinatário
+            Address[] toUser = InternetAddress.parse(StudentControl.student.getEmail());
+
+            message.setRecipients(Message.RecipientType.TO, toUser);
+            message.setSubject("Novas Questões no Estude Aqui!");//Assunto
+            message.setText("Novas Questões Adicionadas! Agora o total de questoes são: " + questions.getState() + "\n\n"
+                    + "Tenha um ótimo dia e uma boa sessão de estudo!");
+            /**
+             * Método para enviar a mensagem criada
+             */
+            Transport.send(message);
+
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
